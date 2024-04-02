5daf7f54859bbff2982b758879677a5f654d1f65
Sainath Motlakunta <m.sainathgupta@gmail.com>
2024-04-02 23:36:58 +0000

    next config handles wasm files

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,21 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 
-const nextConfig = {
-    webpack: (config) => {
-      config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
-      config.module.rules.push({
-        test: /\.wasm$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/chunks/[name].[hash][ext]',
-        },
-      });
-      config.resolve.fallback = { fs: false };
-      return config;
+export function webpack(config) {
+  // Add rule for handling .wasm files
+  config.module.rules.push({
+    test: /\.wasm$/,
+    type: 'asset/resource',
+    generator: {
+      publicPath: './.next/',
+      filename: '[name]-[hash][ext]',
     },
-  
-    experimental: {
-      optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-    },
-  };
+  });
 
-export default nextConfig;
+
+  // Adjust externals and fallback configurations
+  config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
+  config.resolve.fallback = { fs: false };
+
+  return config;
+}
+export const output = 'export';
